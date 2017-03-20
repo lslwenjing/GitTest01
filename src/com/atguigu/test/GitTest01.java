8f511e28b6b83a192c7e8fb248aa7bd57c3ce7da
lslwenjing <2228343958@qq.com>
2017-03-20 13:14:31 +0800

    Update GitTest01.java

--- src/com/atguigu/test/GitTest01.java
+++ src/com/atguigu/test/GitTest01.java
@@ -7,6 +7,7 @@ public class GitTest01 {
 		System.out.println("222222222222");
 		System.out.println("333333333333");
 		System.out.println("444444444444");
+		System.out.println("444444444444");
 	}
 
 }
