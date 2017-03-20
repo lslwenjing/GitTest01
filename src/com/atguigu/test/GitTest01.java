736bde570d0d725831f7fe912f30bcbda1d3556b
lslwenjing <2228343958@qq.com>
2017-03-20 13:21:51 +0800

    update file06

--- src/com/atguigu/test/GitTest01.java
+++ src/com/atguigu/test/GitTest01.java
@@ -9,6 +9,7 @@ public class GitTest01 {
 		System.out.println("444444444444");
 		System.out.println("555555555555");
 		System.out.println("444444444444");
+		System.out.println("666666666666");
 	}
 
 }
